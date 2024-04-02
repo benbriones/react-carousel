583916f5156806af0ce622257d4ce1e294844878
Ben Briones <benbriones50@gmail.com>
2024-04-02 15:51:44 -0700

    finsihed steps 1-3

---
+++ src/Card.test.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { describe, test, expect } from "vitest";
+import { render, fireEvent } from "@testing-library/react";
+import Carousel from "./Carousel";
+import Card from "./Card";
+
+
+describe("Card Component", function () {
+    test("renders", function () {
+        render(<Card
+            caption="testCap"
+            src="test.png"
+            currNum={2}
+            totalNum={3} />
+        );
+    });
+
+    test("matches snapshot", function () {
+        const {container} = render(<Card
+            caption="testCap"
+            src="test.png"
+            currNum={2}
+            totalNum={3} />
+        );
+
+        expect(container).toMatchSnapshot();
+      });
+});
